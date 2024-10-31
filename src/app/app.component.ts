b8e0ef40beb50bcc7c62fbf9a4409f2c1fb524d1
ruchithaparmesh2 <ruchithaparmeshwar2@gmail.com>
2024-10-31 11:14:16 +0530

    changes are commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from "./hero/hero.component";
+import {FeaturesComponent} from "./features/features.component"
+import { FooterComponent } from './footer/footer.component';
+import { EmailComponent } from './email/email.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent,FeaturesComponent, FooterComponent,EmailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { FeaturesComponent } from './features/features.component';
+import { EmailComponent } from './email/email.component';
+import {  NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+    path:'features',
+    component:FeaturesComponent
+    },
+    {
+        path:'email',
+        component:EmailComponent
+    }
+];
+
